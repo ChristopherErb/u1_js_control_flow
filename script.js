1be67cb24fd47d5415cb800ac9e46da214f3d02b
Christopher Erb <christophererb01@gmail.com>
2023-05-09 13:48:31 -0400

    Mid-commit

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ If statements
 Syntax: 
 if () {}
 
+When checking conditions, == or === are needed.  Using = re-assigned value of veriable
 
+== Just measure content value... '1989' = 1989
+
+=== Measures value and data type... '1989' != 1989
+
+
+true == 'true'  (yes, it does bc ==)
+true === 'true' (false,  triple equals checks the data types.. boolean and string here respectively)
+
+
+if (ageInYears >= 25)
+{
+console.log("yes")
+}
+{
+console.log("no")
+}
 
 */
 
 let ageInYears = 20;
 
-if 
+
