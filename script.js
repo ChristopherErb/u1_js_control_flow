79269d5c632ae83f7b353e0d108846183213e9a8
Christopher Erb <christophererb01@gmail.com>
2023-05-09 14:03:13 -0400

    cat

--- script.js
+++ script.js
@@ -23,8 +23,42 @@ console.log("yes")
 console.log("no")
 }
 
-*/
+
 
 let ageInYears = 20;
 
+const dayOfTheWeek = "Friday";
 
+if (dayOfTheWeek === "Tuesday")
+{
+    console.log("Today is tuesday")
+}
+else if (dayOfTheWeek === "Saturday")
+{
+    console.log("today is saturday")
+}
+else
+{
+    console.log("Enter a different day")
+}
+
+*/
+
+
+const yourMoney = 100
+const catPrice = 100
+
+
+
+if (yourMoney === catPrice)
+{
+    console.log("You have just enough to buy the cat")
+}
+else if (yourMoney > catPrice)
+{
+    console.log(`You have enough to buy the cat with ${yourMoney - catPrice} left over.`)
+}
+else 
+{
+    console.log(`You don't have enough to buy the cat, you need ${catPrice - yourMoney} more dollars.`)
+}
